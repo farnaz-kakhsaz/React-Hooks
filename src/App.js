86a52398958d69098517f9f2b61d2a570bb4351b
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-10-14 18:04:53 +0330

    add HookCounterThree

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-import ClassCounter from "./components/ClassCounter";
-import HookCounter from "./components/HookCounter";
+// import ClassCounter from "./components/ClassCounter";
+// import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
+import HookCounterThree from "./components/HookCounterThree";
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      {/* <HookCounterTwo /> */}
+      <HookCounterThree />
     </div>
   );
 }
