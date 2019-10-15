7665b4e51e009fa956b2dc687f2e94bee331a908
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-10-15 17:38:51 +0330

    section 8

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from "react";
 
 function HookCounterOne() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+    console.log(1);
+  }, [count]);
 
-  return <button onClick={() => setCount(count + 1)}>Click {count}</button>;
+  return (
+    <div>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => setCount(count + 1)}>Click {count}</button>
+    </div>
+  );
 }
 
 export default HookCounterOne;
