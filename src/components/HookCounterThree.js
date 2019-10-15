b136ceeac75f993f029b5a11a571c721e593e71d
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-10-15 09:43:03 +0330

    section 4

--- src/components/HookCounterThree.js
+++ src/components/HookCounterThree.js
@@ -7,16 +7,16 @@ function HookCounterThree() {
       <input
         type="text"
         value={name.firstName}
-        onChange={e => setName({ firstName: e.target.value })}
+        onChange={e => setName({ ...name, firstName: e.target.value })}
       />
       <input
         type="text"
         value={name.lastName}
-        onChange={e => setName({ lastName: e.target.value })}
+        onChange={e => setName({ ...name, lastName: e.target.value })}
       />
       <h2>Your first name is -{name.firstName}</h2>
       <h2>Your last name is -{name.lastName}</h2>
-      <h2>Your last name is -{name.lastName}</h2>
+      <h2>Your last name is -{JSON.stringify(name)}</h2>
     </form>
   );
 }
