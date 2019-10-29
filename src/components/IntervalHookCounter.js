ca11f1b0165a4c9a8fa42c9ca9122c28e1319b23
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-10-29 22:10:29 +0330

    section 11

--- src/components/IntervalHookCounter.js
+++ src/components/IntervalHookCounter.js
@@ -1,7 +1,25 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 
 export default function IntervalHookCounter() {
     const [count, setCount] = useState(0)
-    return <div> {count} </div>
+
+    function counter() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    useEffect(() => {
+        const interval = setInterval(counter, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+
+    return <div>
+        Functional Component:
+    <br />
+        {count}
+    </div>
 }
