a16293d9e108d5ca1441e6500f05e6f1bcd0e3b9
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-10-30 22:06:48 +0330

    section 13

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,20 +3,37 @@ import axios from "axios"
 
 
 export default function DataFetching() {
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState([])
+    const [id, setId] = useState(1)
+    const [postId, setPostId] = useState(1)
+
+    function handleClick() {
+        setPostId(id)
+    }
+
+    function handleChange(event) {
+        const target = event.target
+        setId(target.value)
+    }
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-            console.log(response)
-            setPosts(response.data)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(response => {
+            console.log(response.data)
+            setPost(response.data)
         }
         ).catch(
             error =>
                 console.log(error)
         )
-    }, [])
-    return <ul>
-        {posts.map(item => <li key={item.id}>{item.title}</li>)}
-    </ul>
+    }, [id])
 
+    return (
+        <>
+            <input type="text" value={id} onChange={handleChange} />
+            <button onClick={handleClick}>send</button>
+            <div>
+                {post.title}
+            </div>
+        </>
+    )
 }
